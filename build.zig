c2ab7370004843db38094c708e82d191b2912399
dcyun <Dominic_Duan@163.com>
2024-08-11 18:52:36 +0800

    update to zig 0.13

--- build.zig
+++ build.zig
@@ -17,14 +17,14 @@ pub fn build(b: *std.Build) void {
 
     _ = b.addModule("uuid", .{
         //
-        .root_source_file = .{ .path = "uuid.zig" },
+        .root_source_file = b.path("uuid.zig"),
     });
 
     const lib = b.addStaticLibrary(.{
         .name = "uuid",
         // In this case the main source file is merely a path, however, in more
         // complicated build scripts, this could be a generated file.
-        .root_source_file = .{ .path = "uuid.zig" },
+        .root_source_file = b.path("uuid.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -37,7 +37,7 @@ pub fn build(b: *std.Build) void {
     // Creates a step for unit testing. This only builds the test executable
     // but does not run it.
     const lib_tests = b.addTest(.{
-        .root_source_file = .{ .path = "uuid.zig" },
+        .root_source_file = b.path("uuid.zig"),
         .target = target,
         .optimize = optimize,
     });
